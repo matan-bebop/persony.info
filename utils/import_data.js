4dfe6feff6447797df26d5ddb73d9590f57b3022
Yura Fedoriv <yurko.fedoriv@gmail.com>
2014-03-04 00:45:17 +0200

    fixed: import data script was not verbose and did not exit after job was done

--- utils/import_data.js
+++ utils/import_data.js
@@ -1,46 +1,73 @@
 var fs = require('fs'), file,
     orm = require('orm'),
     settings = require("../conf/settings"),
-    collectModels = function(){
+    collectModels = function () {
         var apps = settings.installed_apps, app,
             models = [];
-        for(var i= 0,l=apps.length;i<l;i++){
+        for (var i = 0, l = apps.length; i < l; i++) {
             app = apps[i];
             models.push(app);
         }
         return models
     },
-        importEntity = function(Entity, name){
-        file = './testdata/'+name+'.json';
+    importEntity = function (Entity, name, finalise) {
+        file = './testdata/' + name + '.json';
         fs.readFile(file, 'utf8', function (err, data) {
-            var item;
+            var item, finished, processed = 0;
             if (err) {
                 console.log('Error: ' + err);
                 return;
             }
             data = JSON.parse(data);
-            for(var i= 0,l=data.length;i<l;i++){
+
+            finished = function () {
+                processed += 1;
+                if (processed == data.length) {
+                    console.log(name, " processed");
+                    if (typeof finalise == 'function') {
+                        finalise();
+                    }
+                }
+            };
+
+            for (var i = 0, l = data.length; i < l; i++) {
                 item = data[i];
 
                 delete item.id;
-                Entity.create(item, function(err, results) {
-                    if(err){console.log(err)}
+                Entity.create(item, function (err, results) {
+                    if (err) {console.log(err)}
+                    finished();
                 });
             }
         });
     };
+
 var connection = null;
 
 function setup(db, cb) {
-    var models = collectModels();
-    for(var i= 0,l=models.length;i<l;i++){
-        try{
-            var Entity = require("../components/"+models[i]+"/models").register(orm, db);
-            cb(Entity, models[i]);
-        }catch(e){}
+    var models = collectModels(), finished, processed = 0;
+
+    finished = function () {
+        processed += 1;
+        if (processed == models.length) {
+            console.log("data imported");
+            // it seem to not work
+            connection.close();
+            process.exit();
+        }
+    };
+
+    for (var i = 0, l = models.length; i < l; i++) {
+        try {
+            var Entity = require("../components/" + models[i] + "/models").register(orm, db);
+            cb(Entity, models[i], finished);
+        } catch (e) {
+            console.log(e.message);
+            finished();
+        }
     }
 }
-orm.connect(settings.database, function (err, db) {
+connection = orm.connect(settings.database, function (err, db) {
     db.settings.set('instance.returnAllErrors', true);
     setup(db, importEntity);
 });
