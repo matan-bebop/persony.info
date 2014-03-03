517a969201ec8aef625cc298cd3eec1a86c74228
Yura Fedoriv <yurko.fedoriv@gmail.com>
2014-03-04 01:18:17 +0200

    made it possible to override default setting with untracked conf/settings.local.js file

--- conf/settings.js
+++ conf/settings.js
@@ -1,3 +1,14 @@
+/*
+Any settings may be overridden in __dirname + '/settings.local.js' file.
+Persist with same object structure. Everything that does not require override may be omitted
+For example:
+ module.exports = {
+     database   : {
+      user     : "root",
+      password : "my_root_password"
+    }
+ };
+*/
 var settings = {
     DEBUG : true,
     database   : {
@@ -5,8 +16,8 @@ var settings = {
         query    : { pool: true },
         host     : "127.0.0.1",
         database : "personDB",
-        user     : "root", /* paste your local username that has access to db */
-        password : "" /* paste your local password */
+        user     : "root",
+        password : ""
     },
     security_key : "GcDesWWD5cHcuN1TYJwv,dWW+IE7LNgS0UTRD4EAlSdo==",
     installed_apps : [
@@ -16,6 +27,10 @@ var settings = {
         'main'
     ]
 };
+
+var localConfigFile = __dirname + '/settings.local.js';
+if (require('fs').existsSync(localConfigFile)) {
+    require('node.extend')(true, settings, require(localConfigFile));
+}
+
 module.exports = settings;
-
-
