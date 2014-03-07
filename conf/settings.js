86a955413760697f3c071616b847d396b7081ace
Alex Vagin <uarpalex@mail.com>
2014-03-07 15:58:04 +0200

    Clean local personal data
    to override settings file
    
     settings.local.js is used

--- conf/settings.js
+++ conf/settings.js
@@ -16,8 +16,8 @@ var settings = {
         query    : { pool: true },
         host     : "127.0.0.1",
         database : "personDB",
-        user     : "root",
-        password : "ketekl"
+        user     : "",
+        password : ""
     },
     security_key : "GcDesWWD5cHcuN1TYJwv,dWW+IE7LNgS0UTRD4EAlSdo==",
     installed_apps : [
