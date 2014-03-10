73f93c376382080e72b1f5bbdf27545b32f7987c
Yura Fedoriv <yurko.fedoriv@gmail.com>
2014-03-10 14:27:36 +0200

    changed default application port in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 9000
+        port: process.env.PORT || 3000
       },
       dev: {
         options: {
