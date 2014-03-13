faffd80b117c4f3d2ce4d4c403994afd7894559b
uarpalex <uarpalex@gmail.com>
2014-03-13 23:39:51 +0200

    sources http prefix added

--- testdata/templates/source.js
+++ testdata/templates/source.js
@@ -16,7 +16,7 @@ module.exports = function(Faker, items){
     return   {
                 "icon": icon,                    // Тип джерела
                 "title": Faker.Lorem.sentence(),       // Заголовок тексту посилання на джерело та атрибут title в <a> тезі
-                "link": Faker.Internet.domainName(),                     // Посилання на джерело
+                "link": "http://" + Faker.Internet.domainName() + "/#",                     // Посилання на джерело
                 "event_id" : event
             }
 }
