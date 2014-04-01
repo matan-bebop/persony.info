6811d433cafa5a05022430b433622e8a128e5605
Oleg Levisky <levabd@gmail.com>
2014-04-01 08:37:22 +0300

    modified:   app/scripts/controllers/detailsPerson.js

--- app/scripts/controllers/detailsPerson.js
+++ app/scripts/controllers/detailsPerson.js
@@ -60,4 +60,21 @@
             }
         ]
     );
+
+    angular.module('personyApp').controller(
+        'controllers.photoModalCtrl',
+        [
+            '$scope', '$modalInstance', 'image', 'description',
+            function ($scope, $modalInstance, image, description) {
+                $scope.modal = {
+                    image: function () {
+                        return image;
+                    },
+                    description: function () {
+                        return description;
+                    }
+                }
+            }
+        ]
+    );
 }());
