78d4f96a496caf991d6c780294b86561fcd293c2
Svyatoslav Sydorenko <wk.cvs.bitbucket@sydorenko.org.ua>
2014-04-05 23:45:13 +0300

    Hotfix for default listening address

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.require('/components/router')(app);
 
 // Start server
 var port = process.env.PORT || 3000;
-var host = process.env.HOST || '127.0.0.1';
+var host = process.env.HOST || '0.0.0.0';
 app.listen(port, host, function () {
     console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
