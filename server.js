4ad50f53c93400f35d06e38d7d11a2066f5aac84
Oleg Levisky <levabd@gmail.com>
2014-04-07 09:31:07 +0300

    fix modal window

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.require('/components/router')(app);
 
 // Start server
 var port = process.env.PORT || 3000;
-var host = process.env.HOST;
+var host = process.env.HOST || '0.0.0.0';
 app.listen(port, function () {
     console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
