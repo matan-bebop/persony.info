7580bb436484cba10d9e3b9786d79b38094ce968
Oleg Levitsky <levabd@gmail.com>
2014-04-08 19:17:56 +0300

    implement responce

--- config/settings.local.js
+++ config/settings.local.js
@@ -2,5 +2,12 @@ module.exports = {
     database   : {
         user     : "root",
         password : "mysql"
+    },
+    compass:      {
+        project: [__dirname, '..', 'app'].join('/'),
+        sass:    'styles/sass',
+        css:     'styles/compiled',
+        config_file: 'styles/sass/config.rb',
+        logging: true
     }
 };
