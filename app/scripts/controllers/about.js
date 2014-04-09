b096d4bfd9fb750addac3df94d998d1153586a50
Oleg Levisky <levabd@gmail.com>
2014-04-09 07:54:41 +0300

    add tabs to about

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,5 +1,33 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('personyApp').controller('AboutCtrl', ['Page', function (Page) {
-  Page.setTitle('Про проект');
-}]);
+    angular.module('personyApp').controller(
+        'AboutCtrl',
+        ['$scope', 'Page',
+            function ($scope, Page) {
+                Page.setTitle('Про проект');
+
+                $scope.tabs = [{
+                    title: '<i>One</i>',
+                    url: 'one.tpl.html'
+                }, {
+                    title: 'Two',
+                    url: 'two.tpl.html'
+                }, {
+                    title: 'Three',
+                    url: 'three.tpl.html'
+                }];
+
+                $scope.currentTab = 'one.tpl.html';
+
+                $scope.onClickTab = function (tab) {
+                    $scope.currentTab = tab.url;
+                }
+
+                $scope.isActiveTab = function(tabUrl) {
+                    return tabUrl == $scope.currentTab;
+                }
+
+            }]
+    );
+}());
